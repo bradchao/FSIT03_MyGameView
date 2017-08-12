66e4a543213187fa08c1c7bb5ba98e49b1dfa8e4
Chien <chien@gmail.com>
2017-08-12 16:30:38 +0800

    v3

--- app/src/main/java/tw/brad/app/helloworld/mygame/GameView.java
+++ app/src/main/java/tw/brad/app/helloworld/mygame/GameView.java
@@ -10,14 +10,19 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GameView extends View {
     private Bitmap bmpBall;
     private Resources res;
     private Paint paintBall;
     private boolean isInit;
     private int viewW, viewH;
-    private float ballW, ballH;
+    private float ballW, ballH, ballX, ballY, dx, dy;
     private Matrix matrix;
+
+    private Timer timer;
 
     public GameView(Context context) {
         super(context);
@@ -28,18 +33,20 @@ public class GameView extends View {
         paintBall.setAlpha(255);
 
         matrix = new Matrix();
+        timer = new Timer();
     }
     private void init(){
         isInit = true;
         viewW = getWidth(); viewH = getHeight();
         ballW = viewW / 12f; ballH = ballW;
+        dx = viewW / 256f; dy = viewH / 256f;
 
         matrix.reset();
         matrix.postScale(ballW / bmpBall.getWidth(), ballH / bmpBall.getHeight());
         bmpBall = Bitmap.createBitmap(bmpBall,0,0,
                 bmpBall.getWidth(),bmpBall.getHeight(),
                 matrix, false);
-
+        timer.schedule(new BallTask(), 1000, 60);
 
     }
 
@@ -48,7 +55,17 @@ public class GameView extends View {
         super.onDraw(canvas);
         if (!isInit) init();
 
-        canvas.drawBitmap(bmpBall,0,0,paintBall);
+        canvas.drawBitmap(bmpBall,ballX,ballY,paintBall);
 
     }
+
+    private class BallTask extends TimerTask {
+        @Override
+        public void run() {
+            ballX += dx;
+            ballY += dy;
+            postInvalidate();
+        }
+    }
+
 }
